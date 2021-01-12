ca022e547fc07ef42311e66422b846e145e399f7
wilsoncari <mattcariwilson@hotmail.com>
2021-01-12 08:28:50 -0700

    update

---
+++ Lesson 2/assignment-portal.js
@@ -0,0 +1,11 @@
+let d= new Date(document.lastModified);
+
+let lastMonth=d.getMonth()+1;
+let lastDay=d.getDate();
+let lastYear=d.getFullYear();
+
+
+document.getElementById("lastupdated").textContent= lastMonth + "\/" +  lastDay  + "\/" + lastYear + " " + d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes() + ":" + (d.getSeconds()<10?'0':'') + d.getSeconds();
+
+let z=new Date();
+document.getElementById("copyrightyear").textContent=z.getFullYear();
