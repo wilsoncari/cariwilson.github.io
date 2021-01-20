ab0a688d6b6bb7dbace71c75cc9befb18fac7201
wilsoncari <mattcariwilson@hotmail.com>
2021-01-20 14:11:08 -0700

    update

--- weather/weather.js
+++ weather/weather.js
@@ -14,3 +14,12 @@ let lastYear=d.getFullYear();
 document.getElementById("date").textContent= lastMonth + "\/" +  lastDay  + "\/" + lastYear + " " + d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes() 
 let z=new Date();
 document.getElementById("copyrightyear").textContent=z.getFullYear();
+
+
+
+let dayOfWeek=z.getDay();
+function pancakeBanner (){
+    if (dayOfWeek==5){
+    document.getElementById("pancakes").classList.remove('pancakes-hide');
+    }   
+}
