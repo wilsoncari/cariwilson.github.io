acd290c61bcab2def751f6d36628d38bc56cb5c2
wilsoncari <mattcariwilson@hotmail.com>
2021-02-08 16:33:50 -0700

    update

---
+++ weather/lesson06/js/weather.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+let d= new Date(document.lastModified);
+
+let lastMonth=d.getMonth()+1;
+let lastDay=d.getDate();
+let lastYear=d.getFullYear();
+
+
+document.getElementById("date").textContent= lastMonth + "\/" +  lastDay  + "\/" + lastYear + " " + d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes() 
+let z=new Date();
+document.getElementById("copyrightyear").textContent=z.getFullYear();
+
+
+
+let dayOfWeek=z.getDay();
+function pancakeBanner (){
+    if (dayOfWeek==5){
+    document.getElementById("pancakes").classList.remove('pancakes-hide');
+    }   
+}
+
